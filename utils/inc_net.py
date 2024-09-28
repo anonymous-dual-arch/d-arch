5176c0f35af46beda5eca59016ef426b4b165f39
anonymous-dual-arch <kurt1024@foxmail.com>
2024-09-28 21:54:16 +0800

    Update inc_net.py

--- utils/inc_net.py
+++ utils/inc_net.py
@@ -553,3 +553,132 @@ class BiasLayer(nn.Module):
     def get_params(self):
         return (self.alpha.item(), self.beta.item())
 
+class AdaptiveNet(nn.Module):
+    def __init__(self, args, pretrained):
+        super(AdaptiveNet, self).__init__()
+        self.convnet_type = args["convnet_type"]
+        self.TaskAgnosticExtractor , _ = get_convnet(args, pretrained) #Generalized blocks
+        self.TaskAgnosticExtractor.train()
+        self.AdaptiveExtractors = nn.ModuleList() #Specialized Blocks
+        self.pretrained=pretrained
+        self.out_dim=None
+        self.fc = None
+        self.aux_fc=None
+        self.task_sizes = []
+        self.args=args
+
+    @property
+    def feature_dim(self):
+        if self.out_dim is None:
+            return 0
+        return self.out_dim*len(self.AdaptiveExtractors)
+    
+    def extract_vector(self, x):
+        base_feature_map = self.TaskAgnosticExtractor(x)
+        features = [extractor(base_feature_map) for extractor in self.AdaptiveExtractors]
+        features = torch.cat(features, 1)
+        return features
+
+    def forward(self, x):
+        base_feature_map = self.TaskAgnosticExtractor(x)
+        features = [extractor(base_feature_map) for extractor in self.AdaptiveExtractors]
+        features = torch.cat(features, 1)
+        out=self.fc(features) #{logits: self.fc(features)}
+
+        aux_logits=self.aux_fc(features[:,-self.out_dim:])["logits"] 
+
+        out.update({"aux_logits":aux_logits,"features":features})
+        out.update({"base_features":base_feature_map})
+        return out
+                
+        '''
+        {
+            'features': features
+            'logits': logits
+            'aux_logits':aux_logits
+        }
+        '''
+        
+    def update_fc(self,nb_classes):
+        _ , _new_extractor = get_convnet(self.args)
+        if len(self.AdaptiveExtractors)==0:
+            self.AdaptiveExtractors.append(_new_extractor)
+        else:
+            self.AdaptiveExtractors.append(_new_extractor)
+            self.AdaptiveExtractors[-1].load_state_dict(self.AdaptiveExtractors[-2].state_dict())
+
+        if self.out_dim is None:
+            logging.info(self.AdaptiveExtractors[-1])
+            self.out_dim=self.AdaptiveExtractors[-1].feature_dim        
+        fc = self.generate_fc(self.feature_dim, nb_classes)             
+        if self.fc is not None:
+            nb_output = self.fc.out_features
+            weight = copy.deepcopy(self.fc.weight.data)
+            bias = copy.deepcopy(self.fc.bias.data)
+            fc.weight.data[:nb_output,:self.feature_dim-self.out_dim] = weight
+            fc.bias.data[:nb_output] = bias
+
+        del self.fc
+        self.fc = fc
+
+        new_task_size = nb_classes - sum(self.task_sizes)
+        self.task_sizes.append(new_task_size)
+        self.aux_fc=self.generate_fc(self.out_dim,new_task_size+1)
+ 
+    def generate_fc(self, in_dim, out_dim):
+        fc = SimpleLinear(in_dim, out_dim)
+        return fc
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def weight_align(self, increment):
+        weights=self.fc.weight.data
+        newnorm=(torch.norm(weights[-increment:,:],p=2,dim=1))
+        oldnorm=(torch.norm(weights[:-increment,:],p=2,dim=1))
+        meannew=torch.mean(newnorm)
+        meanold=torch.mean(oldnorm)
+        gamma=meanold/meannew
+        print('alignweights,gamma=',gamma)
+        self.fc.weight.data[-increment:,:]*=gamma
+    
+    def load_checkpoint(self, args):
+        if args["init_cls"] == 50:
+            pkl_name = "{}_{}_{}_B{}_Inc{}".format( 
+                args["dataset"],
+                args["seed"],
+                args["convnet_type"],
+                0,
+                args["init_cls"],
+            )
+            checkpoint_name = f"checkpoints/finetune_{pkl_name}_0.pkl"
+        else:
+            checkpoint_name = f"checkpoints/finetune_{args['csv_name']}_0.pkl"
+        checkpoint_name = checkpoint_name.replace("memo_", "")
+        model_infos = torch.load(checkpoint_name)
+        model_dict = model_infos['convnet']
+        assert len(self.AdaptiveExtractors) == 1
+
+        base_state_dict = self.TaskAgnosticExtractor.state_dict()
+        adap_state_dict = self.AdaptiveExtractors[0].state_dict()
+
+        pretrained_base_dict = {
+            k:v
+            for k, v in model_dict.items()
+            if k in base_state_dict
+        }
+
+        pretrained_adap_dict = {
+            k:v
+            for k, v in model_dict.items()
+            if k in adap_state_dict
+        }
+
+        base_state_dict.update(pretrained_base_dict)
+        adap_state_dict.update(pretrained_adap_dict)
+
+        self.TaskAgnosticExtractor.load_state_dict(base_state_dict)
+        self.AdaptiveExtractors[0].load_state_dict(adap_state_dict)
+        self.fc.load_state_dict(model_infos['fc'])
+        test_acc = model_infos['test_acc']
+        return test_acc
